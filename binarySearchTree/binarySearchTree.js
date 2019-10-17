f3abc13461e4947e2d2dfca911ee6bb856960d49
ibutler <ian.butler@dda.kyocera.com>
2019-10-17 09:35:44 -0700

    bst methods

--- binarySearchTree/binarySearchTree.js
+++ binarySearchTree/binarySearchTree.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(val) {
-    this.value = value;
+    this.value = val;
     this.left = null;
     this.right = null;
   }
@@ -10,4 +10,59 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // O(lg(n))
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (newNode.value < currentNode.value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode.left = newNode;
+          return this;
+        }
+      } else if (newNode.value > currentNode.value) {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+        } else {
+          currentNode.right = newNode;
+          return this;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+
+  // O(lg(n))
+  find(val) {
+    let currentNode = this.root;
+    while (currentNode) {
+      if (val === currentNode.value) {
+        return currentNode;
+      } else if (val < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
+  }
 }
+
+let bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(12);
+bst.insert(9);
+bst.insert(7);
+
+console.log(bst.find(122));
